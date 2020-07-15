e1b7e11fe5fa67b04b797439244604ca9d023697
Felipe Huszar <felipe.huszar@passeidireto.com>
2020-07-15 10:13:26 -0300

    Adding new classes form the course

--- src/index.js
+++ src/index.js
@@ -1,44 +1,15 @@
 const express = require('express')
 require('./db/mongoose')
-
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT || 3000
-
-// app.use((req, res, next) => {
-//     if(req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-app.use((req, res, next) => {
-    res.status(503).send('Server is on maintenance, please try again soon')    
-})
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
 app.listen(port, () => {
-    console.log('Server is up on ' + port);  
+    console.log('Server is up on port ' + port)
 })
-
-const jwt = require('jsonwebtoken')
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '0 seconds'})
-    console.log(token);
-    
-    
-    const data = jwt.verify(token, 'thisismynewcourse')
-    console.log(data);    
-    
-}
-
-
-
-
-myFunction()
--- src/models/user.js
+++ src/models/user.js
@@ -1,22 +1,35 @@
-const bcrypt = require('bcryptjs')
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
-        lowercase: true, 
-        unique: true,       
+        lowercase: true,
         validate(value) {
-            if(!validator.isEmail(value)) {
+            if (!validator.isEmail(value)) {
                 throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
             }
         }
     },
@@ -24,19 +37,8 @@ const userSchema = new mongoose.Schema({
         type: Number,
         default: 0,
         validate(value) {
-            if(value < 0) {
-                throw new Error('Age must be a positive integer')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if(value.toLowerCase().includes('password')) {
-                throw new Error('Your password is unsecure, typer another password')
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
             }
         }
     },
@@ -45,12 +47,34 @@ const userSchema = new mongoose.Schema({
             type: String,
             required: true
         }
-    }]
+    }],
+    avatar: {
+        type: Buffer
+    }
+}, {
+    timestamps: true
 })
+
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+})
+
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+    delete userObject.avatar
+
+    return userObject
+}
 
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
 
     user.tokens = user.tokens.concat({ token })
     await user.save()
@@ -61,14 +85,14 @@ userSchema.methods.generateAuthToken = async function () {
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
 
-    if(!user) {
-        throw new Error('Could not login')
+    if (!user) {
+        throw new Error('Unable to login')
     }
 
     const isMatch = await bcrypt.compare(password, user.password)
 
-    if(!isMatch) {
-        throw new Error('Could not login')
+    if (!isMatch) {
+        throw new Error('Unable to login')
     }
 
     return user
@@ -78,10 +102,17 @@ userSchema.statics.findByCredentials = async (email, password) => {
 userSchema.pre('save', async function (next) {
     const user = this
 
-    if(user.isModified('password')) {
+    if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8)
-    }    
+    }
 
+    next()
+})
+
+// Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+    const user = this
+    await Task.deleteMany({ owner: user._id })
     next()
 })
 
